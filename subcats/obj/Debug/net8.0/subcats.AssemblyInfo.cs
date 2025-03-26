5533fc5641d5f1f7328976df968079fe4762307b
KARINA LIZETH ALVAREZ ANDINO <klalvarez@uth.hn>
2025-03-25 22:23:47 -0600

    bueno

--- subcats/obj/Debug/net8.0/subcats.AssemblyInfo.cs
+++ subcats/obj/Debug/net8.0/subcats.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("subcats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7e28832cb599a97a9ff09b9e8ac0f4258275ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf17b163c84abe7e0002058c1d95cfe204362a3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("subcats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("subcats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
